635bbb7bd4f096d44d6b0fd587a65491b20505fe
TsuchidaSanemaru <sanemaru.tsuchida@rakus-partners.co.jp>
2022-02-08 17:48:46 +0900

    扶養人数を更新する機能作成　終了
    EmployeeService 更新完了
    
    EmployeeController 更新完了
    
    detail 更新完了

--- src/main/java/jp/co/sample/form/UpdateEmployeeForm.java
+++ src/main/java/jp/co/sample/form/UpdateEmployeeForm.java
@@ -1,5 +1,28 @@
 package jp.co.sample.form;
 
+/**
+ * @author 土田真丸
+ *
+ */
 public class UpdateEmployeeForm {
-
+	/** ID */
+	private String id;
+	/** 扶養⼈数　*/
+	private Integer dependentsCount;
+	public String getId() {
+		return id;
+	}
+	public Integer getDependentsCount() {
+		return dependentsCount;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public void setDependentsCount(Integer dependentsCount) {
+		this.dependentsCount = dependentsCount;
+	}
+	@Override
+	public String toString() {
+		return "UpdateEmployeeForm [id=" + id + ", dependentsCount=" + dependentsCount + "]";
+	}
 }
