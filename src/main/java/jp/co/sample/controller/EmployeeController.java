039d7afd912c3631e5476f984beb34fa9279ce26
TsuchidaSanemaru <sanemaru.tsuchida@rakus-partners.co.jp>
2022-02-08 16:54:43 +0900

    従業員一覧する機能作成　終了
    UpdateEmployeeForm 終了
    
    EmployeeController 終了
    
    list 終了

---
+++ src/main/java/jp/co/sample/controller/EmployeeController.java
@@ -0,0 +1,38 @@
+package jp.co.sample.controller;
+
+
+
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import jp.co.sample.domain.Employee;
+import jp.co.sample.form.UpdateEmployeeForm;
+import jp.co.sample.service.EmployeeService;
+
+/**
+ * @author 土田真丸
+ *
+ */
+@Controller
+@RequestMapping("/employee")
+public class EmployeeController {
+	@Autowired
+	private EmployeeService service;
+	@ModelAttribute
+	public UpdateEmployeeForm setUpdateEmployeeForm() {
+		return new UpdateEmployeeForm();
+	}
+	
+	//**従業員⼀覧を出⼒する。*/
+	@RequestMapping("/showList")
+	public String showList(Model model) {
+		model.addAttribute("employeeList", service.showList());
+		
+		return "employee/list";
+	}
+}
